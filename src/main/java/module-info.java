8f0d71f5d103f605ebf73aa3c47db063e1e98ae6
Jayson <palkia@hotmail.de>
2023-02-23 13:09:39 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.grafikrechner_t22 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.grafikrechner_t22 to javafx.fxml;
+    exports com.example.grafikrechner_t22;
+}
