37c743967322b7dc02450419d0033c49ba600ce9
Honsaka <84501811+Honsaka@users.noreply.github.com>
2023-03-21 13:15:07 +0100

    Add files via upload

--- src/main/java/com/example/grafikrechner_t22/Polynom_symetrie.java
+++ src/main/java/com/example/grafikrechner_t22/Polynom_symetrie.java
@@ -1,31 +1,30 @@
-package com.example.grafikrechner_t22;
+package com.example.grafikrechner_t22master;
 import java.util.ArrayList;
 
-public class Polynom_symetrie {
+ public class  Polynom_symetrie {
 
     static  void Symetrie(int[] polynom, int polynomlength) {
         int runde = 0;
         ArrayList<Integer> ergebniss = new ArrayList<Integer>();
-        while( polynomlength > runde){;
+        while (polynomlength > runde) {
             if (polynom[runde] % 2 == 0) {
                 ergebniss.add(1);
                 runde += 1;
             } else if (polynom[runde] % 2 == 1) {
-                ergebniss.add(2);
+                ergebniss.add(0);
                 runde += 1;
             }
-            String symetrie;
-            if (ergebniss.contains(1) && ergebniss.contains(0))
-                symetrie = "Unbestimmt";
-            else if (ergebniss.contains(1)) {
-                symetrie = "Achsensymetrisch";
-            }
-            else if (ergebniss.contains(0))
-             symetrie = "Punktsymetrisch";
-            
-            }
         }
+        String symetrie;
+        if (ergebniss.contains(1) && ergebniss.contains(0)) {
+            symetrie = "Unbestimmt";
+        } else if (ergebniss.contains(1)) {
 
+            symetrie = "Achsensymetrisch";
+        } else if (ergebniss.contains(0)) {
+            symetrie = "Punktsymetrisch";
+        }
+    }
 
     public static void main(String[] args) {
         int[] polynom = {0, 2, 4};
