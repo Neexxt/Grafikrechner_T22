9cefbb27d7015181eb9c41e2d15f0e1cd5254ca6
Mehmet1Ender1Kirmit <126458853+Mehmet1Ender1Kirmit@users.noreply.github.com>
2023-04-24 20:40:15 +0200

    Add files via upload

--- src/main/java/com/example/grafikrechner_t22/Koordinatensystem.java
+++ src/main/java/com/example/grafikrechner_t22/Koordinatensystem.java
@@ -1,6 +1,10 @@
 package com.example.lk_grafikrechner;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.transform.Affine;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -11,6 +15,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Koordinatensystem extends Application {
+
 
     @Override
     public void start(Stage primaryStage) {
@@ -26,6 +31,7 @@ public class Koordinatensystem extends Application {
 
         // Zeichne y-Achse
         gc.strokeLine(0, -canvas.getHeight() / 2 + 60, 0, canvas.getHeight() / 2 - 60);
+        final double[] scale = {1.0};
 
         // Füge Skalen an den Achsen hinzu
         gc.setTextAlign(TextAlignment.CENTER);
@@ -39,13 +45,59 @@ public class Koordinatensystem extends Application {
             }
         }
 
+        // Zoom-Funktionen hinzufügen
+        canvas.setOnScroll(scrollEvent -> {
+            double scrollDelta = scrollEvent.getDeltaY();
+            // Berechnung des zoom-faktors basierend auf dem Mausrad-Delta
+            double zoomFactor = Math.exp(0.001 * scrollDelta);
+            // neue Skalierung berechnen
+            scale[0] *= zoomFactor;
+            // Transformation anwenden
+            gc.scale(zoomFactor, zoomFactor);
+        });
+
+        canvas.setOnKeyPressed(keyEvent -> {
+            //zoom out mit der Maus
+            if (keyEvent.getCode() == KeyCode.MINUS || keyEvent.getCode() == KeyCode.SUBTRACT) {
+                scale[0] /= 1.1;
+                gc.scale(0.9, 0.9);
+            }
+            //zoom in mit der Maus
+            if (keyEvent.getCode() == KeyCode.PLUS || keyEvent.getCode() == KeyCode.ADD) {
+                scale[0] *= 1.1;
+                gc.scale(1.1, 1.1);
+            }
+        });
+        canvas.setOnKeyPressed(keyEvent -> {
+//zoom out mit Strg und Mausrad
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.MINUS || keyEvent.getCode() == KeyCode.SUBTRACT) {
+                scale[0] /= 1.1;
+                gc.scale(0.9, 0.9);
+            }
+//zoom in mit Strg und Mausrad
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.PLUS || keyEvent.getCode() == KeyCode.ADD) {
+                scale[0] *= 1.1;
+                gc.scale(1.1, 1.1);
+            }
+        });
+        canvas.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.SPACE) {
+                scale[0] *= 1.1;
+                gc.scale(1.1, 1.1);
+            }
+            if (keyEvent.getCode() == KeyCode.F1) {
+                scale[0] *= 1.1;
+            }
+        });
+
+
         BorderPane root = new BorderPane(canvas);
-        Scene scene = new Scene(root, 400, 400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Scene scene = new Scene(root, 400, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        public static void main (String[] args) {
+            launch(args);
+        }
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
